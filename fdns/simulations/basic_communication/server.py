260bb11d2cf222954cd0dd58dd3001cee652b263
Fernando Cezar <netcriptus@gmail.com>
2013-08-12 22:29:40 -0300

    use 0.0.0.0 for server

--- fdns/simulations/basic_communication/server.py
+++ fdns/simulations/basic_communication/server.py
@@ -5,7 +5,7 @@ sys.path.insert(0, os.path.abspath('../..'))
 from controllers.streamer import Stream
 from SocketServer import ThreadingUDPServer
 
-HOST, PORT = "localhost", 5000
+HOST, PORT = "0.0.0.0", 5000
 
 server = ThreadingUDPServer((HOST, PORT), Stream)
 server.serve_forever()
