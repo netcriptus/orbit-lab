757e718ea2be3557b87b0294d0031f64ae4bdbac
Fernando Cezar <netcriptus@gmail.com>
2013-09-02 23:54:44 -0300

    write data do stdout

--- fdns/simulations/basic_communication/client.py
+++ fdns/simulations/basic_communication/client.py
@@ -8,4 +8,5 @@ client = socket(AF_INET, SOCK_DGRAM)
 HOST, PORT = "192.168.0.3", 5000
 
 for i in range(20):
+    print >> sys.stdout, "Sending message %d\n" % i
     client.sendto("This is a test message\n", (HOST, PORT))
